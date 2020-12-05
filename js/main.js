77dc8874601e8b259b45bbb3a33f59a2f4df23c2
Casey Friday <casey@friday-next.com>
2020-12-04 20:33:19 -0600

    Solutions Day 4

--- js/main.js
+++ js/main.js
@@ -2,15 +2,15 @@
 
 /************************ DAY 1 *****************************/
 
-let expenses = [1438,781,1917,1371,1336,1802,1566,1878,737,1998,1488,1372,1715,1585,1676,1810,1692,1329,1916,1854,1307,1347,1445,1475,1435,1270,1949,1957,1602,1931,1505,1636,1539,1803,1011,1821,1021,1461,1755,1332,1576,1923,1899,1574,1641,1357,1509,1877,1875,1228,1725,1808,1678,1789,1719,1691,1434,1538,2002,1569,1403,1146,1623,1328,1876,520,1930,1633,1990,1330,1402,1880,1984,1938,1898,1908,1661,1335,1424,1833,1731,1568,1659,1554,1323,1344,1999,1716,1851,1313,1531,190,1834,1592,1890,1649,1430,1599,869,1460,1009,1771,1818,1853,1544,1279,1997,1896,1272,1772,1375,1373,1689,1249,1840,1528,1749,1364,1670,1361,1408,1828,1864,1826,1499,1507,336,1532,1349,1519,1437,1720,1817,1920,1388,1288,1290,1823,1690,1331,1564,1660,1598,1479,1673,1553,1991,66,1571,1453,1398,1814,1679,1652,1687,1951,1334,1319,1605,1757,1517,1724,2008,1601,1909,1286,1780,1901,1961,1798,1628,1831,1277,1297,1744,1946,1407,1856,1922,1476,1836,1240,1591,1572,2000,1813,1695,1723,1238,1588,1881,1850,1298,1411,1496,744,1477,1459,1333,1902];
+let expenses = [1438, 781, 1917, 1371, 1336, 1802, 1566, 1878, 737, 1998, 1488, 1372, 1715, 1585, 1676, 1810, 1692, 1329, 1916, 1854, 1307, 1347, 1445, 1475, 1435, 1270, 1949, 1957, 1602, 1931, 1505, 1636, 1539, 1803, 1011, 1821, 1021, 1461, 1755, 1332, 1576, 1923, 1899, 1574, 1641, 1357, 1509, 1877, 1875, 1228, 1725, 1808, 1678, 1789, 1719, 1691, 1434, 1538, 2002, 1569, 1403, 1146, 1623, 1328, 1876, 520, 1930, 1633, 1990, 1330, 1402, 1880, 1984, 1938, 1898, 1908, 1661, 1335, 1424, 1833, 1731, 1568, 1659, 1554, 1323, 1344, 1999, 1716, 1851, 1313, 1531, 190, 1834, 1592, 1890, 1649, 1430, 1599, 869, 1460, 1009, 1771, 1818, 1853, 1544, 1279, 1997, 1896, 1272, 1772, 1375, 1373, 1689, 1249, 1840, 1528, 1749, 1364, 1670, 1361, 1408, 1828, 1864, 1826, 1499, 1507, 336, 1532, 1349, 1519, 1437, 1720, 1817, 1920, 1388, 1288, 1290, 1823, 1690, 1331, 1564, 1660, 1598, 1479, 1673, 1553, 1991, 66, 1571, 1453, 1398, 1814, 1679, 1652, 1687, 1951, 1334, 1319, 1605, 1757, 1517, 1724, 2008, 1601, 1909, 1286, 1780, 1901, 1961, 1798, 1628, 1831, 1277, 1297, 1744, 1946, 1407, 1856, 1922, 1476, 1836, 1240, 1591, 1572, 2000, 1813, 1695, 1723, 1238, 1588, 1881, 1850, 1298, 1411, 1496, 744, 1477, 1459, 1333, 1902];
 
 // Find 2 entries that sum to 2020, and then multiply them
 for (let i = 0; i < expenses.length; i++) {
-    for(let j = 0; j < expenses.length; j++) {
+    for (let j = 0; j < expenses.length; j++) {
         // skip the outer expense (i)
         if (j !== i) {
-            if( expenses[i] + expenses[j] === 2020) {
-                console.log("2 nums that sum 2020 multiplied = " + (expenses[i]*expenses[j]));
+            if (expenses[i] + expenses[j] === 2020) {
+                console.log("2 nums that sum 2020 multiplied = " + (expenses[i] * expenses[j]));
             }
         }
     }
@@ -18,17 +18,17 @@ for (let i = 0; i < expenses.length; i++) {
 
 // Find 3 entries that sum to 2020, and then multiply them
 for (let i = 0; i < expenses.length; i++) {
-    for(let j = 0; j < expenses.length; j++) {
+    for (let j = 0; j < expenses.length; j++) {
         // skip the outer expense (i)
         if (j !== i) {
             for (let k = 0; k < expenses.length; k++) {
                 if (k !== j) {
                     if (expenses[i] + expenses[j] + expenses[k] === 2020) {
-                        console.log("3 nums that sum 2020 multiplied = " + (expenses[i]*expenses[j]*expenses[k]));
+                        console.log("3 nums that sum 2020 multiplied = " + (expenses[i] * expenses[j] * expenses[k]));
                     }
                 }
             }
-            if( expenses[i] + expenses[j] === 2020) {
+            if (expenses[i] + expenses[j] === 2020) {
 
             }
         }
@@ -37,12 +37,12 @@ for (let i = 0; i < expenses.length; i++) {
 
 /************************ DAY 2 *****************************/
 // pull in ../assets/passwords.json and analyze each for correctness
-$.ajax("/advent-of-code/assets/passwords.json").done(function(data, status, jqXhr) {
+$.ajax("/advent-of-code/assets/passwords.json").done(function (data, status, jqXhr) {
     console.log("Request status: " + status);
     let totalPw = 0;
     let totalCorrect = 0;
     let newTotalCorrect = 0;
-    data.forEach(function(pwGroup, index, allDataArr) {
+    data.forEach(function (pwGroup, index, allDataArr) {
         totalPw++; // count total pw in JSON
         // Decode the pw policy
         let policyPw = pwGroup.policy.split(' ');
@@ -63,9 +63,9 @@ $.ajax("/advent-of-code/assets/passwords.json").done(function(data, status, jqXh
         }
 
         // Decide if ONLY ONE of the positions given has a letter match
-        let firstIndex = pwGroup.password.split('')[policyMin-1];
-        let lastIndex = pwGroup.password.split('')[policyMax-1];
-        if (firstIndex === pwLetter || lastIndex=== pwLetter) {
+        let firstIndex = pwGroup.password.split('')[policyMin - 1];
+        let lastIndex = pwGroup.password.split('')[policyMax - 1];
+        if (firstIndex === pwLetter || lastIndex === pwLetter) {
             // at least one of them matches
             if (!(firstIndex === pwLetter && lastIndex === pwLetter)) {
                 // they don't BOTH match, so add to the counter
@@ -78,17 +78,17 @@ $.ajax("/advent-of-code/assets/passwords.json").done(function(data, status, jqXh
     console.log("New total correct pws: " + newTotalCorrect);
 
 })
-    .fail(function(jqXhr, status, error) {
+    .fail(function (jqXhr, status, error) {
         console.log("There was an error: ");
         console.error(error);
     })
-    .always(function() {
+    .always(function () {
         console.log("Finished processing passwords");
     });
 
 /************************ DAY 3 *****************************/
 $.ajax("/advent-of-code/assets/trees.json")
-    .done(function(data, status, jqXhr) {
+    .done(function (data, status, jqXhr) {
 
         // if we go at a slope of -1/3, how many trees(#) will we hit?
         let offset = 0;
@@ -105,9 +105,10 @@ $.ajax("/advent-of-code/assets/trees.json")
         let offset3 = 0;
         let offset4 = 0;
         let offset5 = 0;
-        data.forEach(function(line, index, fullArr) {
+        data.forEach(function (line, index, fullArr) {
             // Now we're looking at a single row of trees
-            let thisRow = line.treeRow.split(''); let hit = "no";
+            let thisRow = line.treeRow.split('');
+            let hit = "no";
             offset = checkOffset(offset);
             offset2 = checkOffset(offset2);
             offset3 = checkOffset(offset3);
@@ -134,11 +135,11 @@ $.ajax("/advent-of-code/assets/trees.json")
         console.log("All trees multiplied together: " + numTreesHit * numTreesHit2 * numTreesHit3 * numTreesHit4 * numTreesHit5);
 
     })
-    .fail(function(jqXhr, status, error) {
+    .fail(function (jqXhr, status, error) {
         console.log("There was an error: ");
         console.error(error);
     })
-    .always(function() {
+    .always(function () {
         console.log("Finished processing trees");
     });
 
@@ -150,10 +151,68 @@ function avoidTrees(row, offset) {
         return 0;
     }
 }
+
 function checkOffset(offset) {
-    if(offset > 30) {
+    if (offset > 30) {
         // the pattern repeats, so break it down to quotient/remainder
         return offset - 31;
     }
     return offset;
 }
+
+/************************ DAY 4 *****************************/
+$.ajax("/advent-of-code/assets/passports.json")
+    .done(function (data, status, jqXhr) {
+        let numValidPassports = 0;
+        data.forEach(function (passport, index, fullArr) {
+            // Looking at individual passports
+            //  byr (Birth Year)
+            if (passport.hasOwnProperty("byr") && parseInt(passport.byr) >= 1920 && parseInt(passport.byr) <= 2002) {
+                //  iyr (Issue Year)
+                if (passport.hasOwnProperty("iyr") && parseInt(passport.iyr) >= 2010 && parseInt(passport.iyr) <= 2020) {
+                    //  eyr (Expiration Year)
+                    if (passport.hasOwnProperty("eyr") && parseInt(passport.eyr) >= 2020 && parseInt(passport.eyr) <= 2030) {
+                        //  hgt (Height)
+                        if (passport.hasOwnProperty("hgt")) {
+                            let unit = passport.hgt.slice(-2);
+                            let height = passport.hgt.substr(0, passport.hgt.length - 2);
+                            if ((unit === 'cm' && height >= 150 && height <= 193) || (unit === 'in' && height >= 59 && height <= 76)) {
+                                //  hcl (Hair Color)
+                                if (passport.hasOwnProperty("hcl")) {
+                                    let hcl = passport.hcl;
+                                    let allowedChar = /^#[0-9A-F]{6}$/i;
+                                    if (hcl.substr(0, 1) === '#' && hcl.substr(1).length === 6 && allowedChar.test(hcl) !== false) {
+                                        //  ecl (Eye Color)
+                                        if (passport.hasOwnProperty("ecl")) {
+                                            let ecl = passport.ecl;
+                                            if (['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(ecl)) {
+                                                //  pid (Passport ID)
+                                                if (passport.hasOwnProperty("pid")) {
+                                                    let pid = passport.pid;
+                                                    if (parseInt(pid) && pid.length === 9) {
+
+                                                        //  cid (Country ID)
+                                                        //  Don't have to check for this
+                                                        numValidPassports++;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        ;
+        console.log("Total valid passports = " + numValidPassports);
+    })
+    .fail(function (jqXhr, status, error) {
+        console.log("There was an error: ");
+        console.error(error);
+    })
+    .always(function () {
+        console.log("Finished processing passports");
+    });
